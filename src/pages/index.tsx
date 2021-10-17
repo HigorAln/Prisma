efb395831f14f74351583b995de62bf5a5bdd660
Higor Allan <higor.allan21@gmail.com>
2021-10-17 12:35:28 -0300

    Initial commit from Create Next App

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <h5>
+        Styled-components <a href="#">OK</a>
+      </h5>
+      <h5>
+        Testing-library <a href="#">OK</a>
+      </h5>
+      <h5>
+        Babel-Config <a href="#">OK</a>
+      </h5>
+    </div>
+  );
+}
